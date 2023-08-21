cd4a423c418e35dbbb80f29a587fc3f68198fc4e
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-08-21 17:46:26 +0330

    declare some component in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChartSelectorComponent } from './charts/chart-selector/chart-selector.component';
+import { ColumnBarChartComponent } from './charts/column-bar-chart/column-bar-chart.component';
+import { StackedBarChartComponent } from './charts/stacked-bar-chart/stacked-bar-chart.component';
+import { ClusteredBarChartComponent } from './charts/clustered-bar-chart/clustered-bar-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartSelectorComponent,
+    ColumnBarChartComponent,
+    StackedBarChartComponent,
+    ClusteredBarChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
