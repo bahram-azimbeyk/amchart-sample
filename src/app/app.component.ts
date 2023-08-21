2261675213629aba6051baa48dcd1b12664b9cb9
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-08-21 17:47:50 +0330

    new logic in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartType } from './models/chart.type';
+import { ChartService } from './services/chart.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private chartService: ChartService) { }
+
+  chartTypes: { name: string; value: ChartType }[] = [
+    { name: 'Regular Bar Chart', value: 'column' },
+    { name: 'Clustered Bar Chart', value: 'clustered' },
+    { name: '100% Stacked Bar Chart', value: 'stacked' }
+  ]
+
+  selectedChartType: ChartType = 'column';
+
+  onSelect(chartType: ChartType): void {
+    if (this.selectedChartType !== chartType) {
+      this.selectedChartType = chartType;
+      this.chartService.setChartType(chartType);
+    }
+  }
 }
